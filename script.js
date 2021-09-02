b1283d08e28b0347aa39bff52d90806bc485e99f
iCarambaaa <dkpress1916@zoho.eu>
2021-09-02 18:18:12 +0200

    new

--- script.js
+++ script.js
@@ -15,23 +15,55 @@ function addToList() {
     console.log(people)
 }
 
+// ---USE ENTER TO TRIGGER ADD MEMBER BUTTON---
+
+
+document.getElementById("person").addEventListener("keyup", function(event) {
+
+      
+    if (event.key === "Enter") {
+      // Trigger the button element with a click
+      document.getElementById("button-add-to-list").click()
+    }
+});
+
+
 // ---CREATE TEAMS AS COLUMNS---
 
 function createTeams() {
     let num = document.getElementById("teams").value
-    let num2 = parseInt(num)
-    for (let i = 0; i < num2; i++) {
+    num = parseInt(num)
+    document.getElementById("teamsrow").innerHTML = ""
+    for (let i = 1; i <= num; i++) {
         let col = document.createElement("div")
-        col.classList.add("col")
-        col.classList.add("teamcol")
+        col.classList.add("col", "teamcol")
         document.getElementById("teamsrow").appendChild(col)
         document.getElementById("teams").value = ''
-        col.innerHTML = "Team " + i
+        //let ul = document.createElement("ul")
+        //ul.setAttribute("id", "memberUl")
+        col.innerHTML = "Team " + i //+ ul
     }
 }
 
+// ---USE ENTER TO TRIGGER CREATE TEAMS BUTTON---
+
+
+document.getElementById("teams").addEventListener("keyup", function(event) {
+
+      
+    if (event.key === "Enter") {
+
+      // Trigger the button element with a click
+      document.getElementById("button-create-teams").click()
+    }
+});
+
+
 // ---ASSIGN PERSON FROM LIST TO TEAM---
 
-function assignRandomly() {
-
-}
+// function assignRandomly() {
+//    let one = people.pop()
+//    let li = document.createElement("li")
+//    li.innerText = one
+//    document.getElementById("memberUl").appendChild(li)
+// }
